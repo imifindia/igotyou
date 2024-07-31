8f2c0e0b0a0b160ce4e24394afca996c1f48f6b4
Sooraj Rajeevan <soorajerajeevan@yahoo.com>
2024-07-31 23:24:31 +0530

    Removed ExpressJS Backend as we decided to move to Python Backend.

--- igotyou-backend/middleware/notFoundHandler.js
+++
@@ -1,6 +0,0 @@
-const notFoundHandler = (req, res, next) => {
-    res.status(404).json({ error: 'Not Found', message: 'The requested resource could not be found' });
-  };
-  
-  module.exports = notFoundHandler;
-  
