c4e6134ca3f3fc69db275ced6fdb3e814da1eb2c
Vijesh V <viji07denovo@gmail.com>
2024-08-03 21:17:38 +0530

    updated family name field

--- frontend/report.js
+++ frontend/report.js
@@ -20,7 +20,7 @@ function displayData(apiData) {
         { index: 3, title: "False I don't Agree", variable: "down_vote", cellgenerator: "voteGenerator" },
         { index: 4, title: "Name", variable: "name" },
         { index: 5, title: "Nickname", variable: "nickname" },
-        { index: 6, title: "Family Name", variable: "familyname" },
+        { index: 6, title: "Family Name", variable: "familyName" },
         { index: 7, title: "Age", variable: "age" },
         { index: 8, title: "Sex", variable: "sex" },
         { index: 9, title: "Place", variable: "place" },
